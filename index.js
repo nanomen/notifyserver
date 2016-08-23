29c301d57c7bbf6034bc58e2e72650e6462ccda6
Aleksey <nanomen@mail.ru>
2016-08-23 11:55:59 +0300

    fix

--- index.js
+++ index.js
@@ -6,25 +6,15 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html') );
 
 // Пдключились
 io.on('connection', (socket) => {
-
   console.log('Client connected');
-
   socket.on('disconnect', () => console.log('Client disconnected'));
 
+
+  io.emit('settime', new Date().toTimeString());
   socket.on('gettime', () => {
     io.emit('settime', new Date().toTimeString());
   });
+
 });
-
-// Посылаем события
-//setTime();
-io.emit('settime', new Date().toTimeString());
-
-
-//io.on('gettime', setTime);
-
-/*function setTime() {
-  io.emit('settime', new Date().toTimeString());
-}*/
 
 http.listen((process.env.PORT || 5000), () => console.log('listening on *:' + (process.env.PORT || 5000)));
