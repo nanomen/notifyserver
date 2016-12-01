845d0ea45c2f7ac1ea0872eb0662be155106a0c2
Alexey Chernyshev <chernyshev@rg.ru>
2016-12-01 11:02:52 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/scripts'));
+app.use(app.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 
